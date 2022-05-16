be19c777e33c6ce24c65a6b7ff3614670cbe30f1
Martin Dupuis <4422549+mdupuis13@users.noreply.github.com>
2022-05-15 21:19:03 -0400

    Initial commit

---
+++ src/main/kotlin/info/martindupuis/tradingclient/TradingclientApplication.kt
@@ -0,0 +1,11 @@
+package info.martindupuis.tradingclient
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TradingclientApplication
+
+fun main(args: Array<String>) {
+	runApplication<TradingclientApplication>(*args)
+}
---
+++ src/test/kotlin/info/martindupuis/tradingclient/TradingclientApplicationTests.kt
@@ -0,0 +1,13 @@
+package info.martindupuis.tradingclient
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TradingclientApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
