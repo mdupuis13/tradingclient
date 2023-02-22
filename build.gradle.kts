a86f2eb9700cd13fbf7a2f219c312f2d19281580
Martin Dupuis <4422549+mdupuis13@users.noreply.github.com>
2023-02-21 20:23:54 -0500

    Add automatic dependency version checker

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.6.7"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("com.github.ben-manes.versions") version "0.46.0"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
 	kotlin("plugin.jpa") version "1.6.21"
