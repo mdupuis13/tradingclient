9a71750c474fd97a6c920f910dfb4183327acc6c
Martin Dupuis <4422549+mdupuis13@users.noreply.github.com>
2023-02-27 20:38:21 -0500

    Upgrade to java 19 target

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 group = "info.martindupuis"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_19
 
 repositories {
 	mavenCentral()
@@ -40,7 +40,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
+		jvmTarget = "19"
 	}
 }
 
