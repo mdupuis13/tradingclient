192cf979b61c784f5ec97be4eea7319313697e2a
Martin Dupuis <4422549+mdupuis13@users.noreply.github.com>
2023-03-25 07:07:08 -0400

    Remove example refresh-token

--- src/main/kotlin/info/martindupuis/tradingclient/portsadapters/questradeclient/entities/QuestradeRefreshToken.kt
+++ src/main/kotlin/info/martindupuis/tradingclient/portsadapters/questradeclient/entities/QuestradeRefreshToken.kt
@@ -5,11 +5,3 @@ class QuestradeRefreshToken(
         val api_server: String,
         val refresh_token: String) {}
 
-/*
-{
-    "access_token": "9SL7_TBDglIkns25VvRCJR8DARzyzRpQ0",
-    "api_server": "https://api07.iq.questrade.com/",
-    "expires_in": 1800,
-    "refresh_token": "53bHzILSSv1O6KLvE0rFA1ibJcHi7gKc0",
-    "token_type": "Bearer"
-}*/
