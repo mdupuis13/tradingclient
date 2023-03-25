8a4daa1710eaa416ef9ac227f78425948cf86d98
Martin Dupuis <4422549+mdupuis13@users.noreply.github.com>
2023-03-24 23:05:36 -0400

    Remove unnecessary mock

--- src/test/kotlin/info/martindupuis/tradingclient/portsadapters/questradeclient/QuestradeServiceTests.kt
+++ src/test/kotlin/info/martindupuis/tradingclient/portsadapters/questradeclient/QuestradeServiceTests.kt
@@ -1,9 +1,6 @@
 package info.martindupuis.tradingclient.portsadapters.questradeclient
 
-import com.jquestrade.Authorization
-import com.jquestrade.Questrade
 import info.martindupuis.tradingclient.portsadapters.questradeclient.entities.QuestradeRefreshToken
-import io.mockk.OfTypeMatcher
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
@@ -11,7 +8,6 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.Mock
 
 private const val REFRESH_TOKEN = "abc123"
 
@@ -19,9 +15,6 @@ private const val REFRESH_TOKEN = "abc123"
 internal class QuestradeServiceTests {
     @MockK
     lateinit var questradeRepo: QuestradeRepository
-
-    @MockK
-    lateinit var jquestradeLib: Questrade
 
     private lateinit var sut: QuestradeService
 
@@ -39,7 +32,6 @@ internal class QuestradeServiceTests {
     @Test
     fun whenIcallConnect_thenTheServiceConnectsToQuestradeAPI() {
         every { questradeRepo.getRefeshToken() } returns QuestradeRefreshToken("", "", REFRESH_TOKEN)
-//        every { constructedWith<Questrade>(OfTypeMatcher<String>(String::class)).retrieveAccessToken() } returns REFRESH_TOKEN
 
         sut.connect()
         assertTrue(sut.isConnected())
