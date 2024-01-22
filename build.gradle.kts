47fc7b861356a9b53ef9b416c102102b198ee925
Martin Dupuis <4422549+mdupuis13@users.noreply.github.com>
2024-01-21 20:57:58 -0500

    Formatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,68 +1,70 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.2.1"
-	id("io.spring.dependency-management") version "1.1.4"
-	id("com.github.ben-manes.versions") version "0.50.0"
-	id("org.flywaydb.flyway") version "9.22.1"
+    id("org.springframework.boot") version "3.2.1"
+    id("io.spring.dependency-management") version "1.1.4"
+    id("com.github.ben-manes.versions") version "0.50.0"
+    id("org.flywaydb.flyway") version "9.22.1"
 
-	kotlin("plugin.spring") version "1.9.20"
-	kotlin("jvm") version "1.9.20"
-	kotlin("plugin.jpa") version "1.9.21"
-	kotlin("kapt") version "1.9.21"
+    kotlin("plugin.spring") version "1.9.20"
+    kotlin("jvm") version "1.9.20"
+    kotlin("plugin.jpa") version "1.9.21"
+    kotlin("kapt") version "1.9.21"
 }
 
 group = "info.martindupuis"
 version = "0.0.1-SNAPSHOT"
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-web-services")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-	implementation("org.flywaydb:flyway-core")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-	
-	implementation(project(":lib:j-questrade"))
-	testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-web-services")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 
-	runtimeOnly("org.postgresql:postgresql")
+    implementation(project(":lib:j-questrade"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
 
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	
-	testImplementation(kotlin("test"))
-	testImplementation("org.amshove.kluent:kluent:1.73")
-	// To get JUnit errors from kotlin.test, to e.g. enable diff windows in failure messages
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.9.21")
-	testImplementation("com.h2database:h2")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("io.mockk:mockk:1.13.8")
-	testImplementation("io.projectreactor:reactor-test")
+    runtimeOnly("org.postgresql:postgresql")
 
-	kapt("org.springframework.boot:spring-boot-configuration-processor")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    testImplementation(kotlin("test"))
+    testImplementation("org.amshove.kluent:kluent:1.73")
+    // To get JUnit errors from kotlin.test, to e.g. enable diff windows in failure messages
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.9.21")
+    testImplementation("com.h2database:h2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.mockk:mockk:1.13.8")
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("org.instancio:instancio-junit:3.6.0")
+    testImplementation("org.junit-pioneer:junit-pioneer:2.2.0")
+
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
 }
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "21"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "21"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
