051388a756fc57d45a75391cfbc0176a62f45b28
Martin Dupuis <4422549+mdupuis13@users.noreply.github.com>
2024-01-21 21:01:46 -0500

    Remove unnecessary class due to client cange

--- src/test/kotlin/info/martindupuis/tradingclient/portsadapters/questradeclient/ServiceTests.kt
+++ src/test/kotlin/info/martindupuis/tradingclient/portsadapters/questradeclient/ServiceTests.kt
@@ -2,7 +2,6 @@ package info.martindupuis.tradingclient.portsadapters.questradeclient
 
 
 import com.jquestrade.Account
-import com.jquestrade.AuthenticationToken
 import info.martindupuis.tradingclient.portsadapters.questradeclient.entities.QuestradeRefreshToken
 import io.mockk.MockKAnnotations
 import io.mockk.every
@@ -14,7 +13,6 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import java.time.ZonedDateTime
 import com.jquestrade.client.QuestradeWebClient as LibQuestrade
 
 private const val REFRESH_TOKEN = "abc123"
@@ -61,26 +59,6 @@ internal class ServiceTests {
         val myAccounts = sut.getAccounts()
 
         assertThat(myAccounts.size).isEqualTo(1)
-    }
-
-
-    /**
-     *  This class is created because the j-questrade library has not been
-     *  created with testing in mind :/
-     *
-     *  We have to create objects using a json to object mapper (jackson-module-kotlin in this case)
-     */
-    inner class JQuestradeTestData {
-        fun createBasicAccountsArray(): List<Account> {
-            val anAccount = Account("Cash", "1", "Active", true, true, "Individual")
-
-            return listOf(anAccount)
-        }
-
-        fun createAuthorizationToken(): AuthenticationToken{
-            return AuthenticationToken("eidohch4Oom2ieNgetooLeiF4ix1iequo", "https://api07.iq.questrade.com/",
-                ZonedDateTime.now() , "baeNoo1cie0Daiyo6at7seisah3Ohweef", "Bearer")
-        }
     }
 }
 
