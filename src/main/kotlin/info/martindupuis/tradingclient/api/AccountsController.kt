41d1b93780e9f9ac88f71d5bb8950ca0a48b64e5
Martin Dupuis <4422549+mdupuis13@users.noreply.github.com>
2024-08-31 21:42:35 -0400

    Have a functioning AccountsController

--- src/main/kotlin/info/martindupuis/tradingclient/api/AccountsController.kt
+++ src/main/kotlin/info/martindupuis/tradingclient/api/AccountsController.kt
@@ -2,12 +2,21 @@ package info.martindupuis.tradingclient.api
 
 import info.martindupuis.jquestrade.Account
 import info.martindupuis.tradingclient.portsadapters.questradeclient.TradingService
+import info.martindupuis.tradingclient.portsadapters.questradeclient.entities.QuestradeRefreshToken
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.time.ZonedDateTime
 
 @RestController
 class AccountsController(val service: TradingService) {
 
     @GetMapping("/tradingclient/api/accounts")
-    fun accounts(): List<Account> = service.getAccounts()
+    fun accounts(@RequestParam("refresh_token") refreshToken: String): List<Account> {
+        if (!service.isConnected())
+            service.connect(QuestradeRefreshToken("", "", ZonedDateTime.now(), refreshToken))
+
+        return service.getAccounts()
+    }
+
 }
