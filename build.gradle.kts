d197ba04db987287d84a7ac342fcadfef30a6d60
Martin Dupuis <4422549+mdupuis13@users.noreply.github.com>
2024-09-13 21:56:27 -0400

    Remove lombok from kotlin config

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
     runtimeOnly("org.postgresql:postgresql")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-    implementation("org.projectlombok:lombok:1.18.28")
+    //implementation("org.projectlombok:lombok:1.18.28")
 
     implementation("org.mapstruct:mapstruct:1.5.5.Final")
     kapt("org.mapstruct:mapstruct-processor:1.5.5.Final")
