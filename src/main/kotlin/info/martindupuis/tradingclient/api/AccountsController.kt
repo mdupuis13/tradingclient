1dc0c5e81ed7243d8f576a2da227991f8986f2e7
Martin Dupuis <4422549+mdupuis13@users.noreply.github.com>
2024-09-24 19:47:16 -0400

    PoC for getting account activities
    Refactor to follow

--- src/main/kotlin/info/martindupuis/tradingclient/api/AccountsController.kt
+++ src/main/kotlin/info/martindupuis/tradingclient/api/AccountsController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDate
-import java.time.LocalDateTime
+
 import java.time.ZonedDateTime
 
 @RestController
